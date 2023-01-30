70a93b74242c25e87918b070ca2b70154c5c4496
jingyig16 <jingyig616@gmail.com>
2023-01-30 16:31:14 -0500

    updates

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 
 // JS File for ic-05
 
+function clickedTimes(){
+	let newText = "Number of times button clicked:";
+	let buttonDiv = document.getElementById("button-div");
+
+	buttonDiv.innerHTML = newText + console.count();
+}
+
