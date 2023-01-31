7ff6c991f0f80ea75963865fd280baa4bb38cc22
jingyig16 <jingyig616@gmail.com>
2023-01-30 21:01:25 -0500

    comments

--- main.js
+++ main.js
@@ -5,9 +5,11 @@ let numCount = 0
 
 function clickedTimes(){
 
+	// count number of time clicked
 	numCount += 1;
 	let countTracker = "Number of times button clicked: " + numCount;
 	
+	// change the text
 	let buttonDiv = document.getElementById("button-div");
 
 	buttonDiv.innerHTML = countTracker; 
